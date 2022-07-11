733507958f41cda8a99640a68a1d658584c9bdcd
levijb <levijbdavis@gmail.com>
2022-07-11 11:07:03 -0400

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Module 14',
+    version='1.0.0',
+    url='https://github.com/levijb/HW14.git',
+    author='Levi Davis',
+    author_email='ljd3frf@virginia.edu',
+    description='M14 Class Challenge Package',
+    packages=find_packages(),    
+    install_requires=['pandas', 'unittest'],
+)
